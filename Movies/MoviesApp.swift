e18f64f11918938f7b932c3583f21ca7f5fdafe8
Tarek Radovan <tarekradovan@Tareks-MacBook-Pro.local>
2025-09-12 18:16:28 -0400

    first commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Tarek Radovan on 12/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoviesTests/MoviesTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesTests.swift
+//  MoviesTests
+//
+//  Created by Tarek Radovan on 12/09/2025.
+//
+
+import Testing
+@testable import Movies
+
+struct MoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
